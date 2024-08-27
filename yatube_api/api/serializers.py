8340e16fd9d9e932d4931ef95e2da4609e11c802
Max Stolpovskikh <maximstolpovskikh@gmail.com>
2024-08-27 23:26:32 +0700

    update serializers.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -12,13 +12,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = (
-            'id',
-            'author',
-            'post',
-            'text',
-            'created'
-        )
+        fields = '__all__'
         read_only_fields = ('author', 'post')
 
 
@@ -33,24 +27,11 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = (
-            'id',
-            'text',
-            'author',
-            'image',
-            'group',
-            'pub_date',
-            'comments',
-        )
+        fields = '__all__'
 
 
 class GroupSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Group
-        fields = (
-            'id',
-            'title',
-            'slug',
-            'description',
-        )
+        fields = '__all__'
